553dacd6b465e831fcb1fcb479d8c27cd483f753
quangvungo <quangvu221004@gmail.com>
2026-04-05 10:15:18 +0700

    feat(certification): complete CertificationTitle component

--- app/(page)/student/certification/CertificationTitle/CertificationTitle.tsx
+++ app/(page)/student/certification/CertificationTitle/CertificationTitle.tsx
@@ -1,7 +1,8 @@
 export default function CertificationTitle() {
     return (
-        <div className="mt-10">
-            Hello
+        <div className="mt-5 p-5">
+            <h1 className="text-xl mb-2 font-bold">Nộp chứng chỉ dành cho sinh viên</h1>
+            <p className="text-gray-500 text-sm">Hoàn thành chứng chỉ của bạn bằng cách nộp các yêu cầu từ nhà trường</p>
         </div>
     )
 }
