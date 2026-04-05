c6b2370ec9be459c919d45e4701b57305c021772
quangvungo <quangvu221004@gmail.com>
2026-04-05 16:08:18 +0700

    refactor(studyResult): change type for hook

--- hooks/student/useStudyResult.ts
+++ hooks/student/useStudyResult.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query"
 import { getDetailedStudyResult } from '@/app/api/studentService/studyResult';
-import { IStudyResultData } from '@/app/types/student/studyResult.type';
+import { IStudyResultDataResponse } from '@/app/types/student/studyResult.type';
 
 export const useGetDetailedStudyResult = () => {
-    return useQuery<IStudyResultData[]>({
+    return useQuery<IStudyResultDataResponse>({
         queryKey: ['get-detailed-study-result'],
         queryFn: () => getDetailedStudyResult(),
         staleTime: 5 * 60 * 1000,
