9a4fc214085ea7f83d3a442885e7ec581081b32b
quangvungo <quangvu221004@gmail.com>
2026-04-06 10:54:36 +0700

    refactor(layoutStudent): add Chatbox component

--- app/(page)/student/layout.tsx
+++ app/(page)/student/layout.tsx
@@ -1,8 +1,11 @@
+import ChatBox from "./components/ChatBox/ChatBox";
+
 export default function LayoutStudent({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <>
-            <main className="bg-gray-100 min-h-screen pt-15">
+            <main className="bg-gray-100 min-h-screen pt-15 relative">
                 {children}
+                <ChatBox />
             </main>
         </>
     )
