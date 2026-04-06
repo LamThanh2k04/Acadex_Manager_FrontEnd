d9a349dd3d2512836003fb21b6b8cdc101d1783d
quangvungo <quangvu221004@gmail.com>
2026-04-06 16:23:11 +0700

    refactor(chatbox): fix res.data.data

--- app/api/studentService/chatbox.ts
+++ app/api/studentService/chatbox.ts
@@ -2,5 +2,5 @@ import { https } from "../config"
 import { IChatBoxMessage } from '@/app/types/student/chatbox.type';
 export const chatBoxMessage = async (data: IChatBoxMessage) => {
     const res = await https.post("/api/student/ai/aiMessage", data);
-    return res.data;
+    return res.data.data;
 };
