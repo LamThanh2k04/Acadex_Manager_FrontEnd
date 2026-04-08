70adf9c692fdbdbe3622a90d2fbfda0dad950d00
quangvungo <quangvu221004@gmail.com>
2026-04-08 23:12:37 +0700

    feat(lecturer): add and completed overview for lecturer

--- app/api/lecturerService/dashboard/overView.ts
+++ app/api/lecturerService/dashboard/overView.ts
@@ -1,5 +1,4 @@
 import { https } from "../../config"
-
 export const getOverview = async () => {
     const res = await https.get("/api/lecturer/dashboard/getOverView");
     return res.data.data
--- hooks/lecturer/dashboard/useGetOverview.ts
+++ hooks/lecturer/dashboard/useGetOverview.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
-import { getOverview } from '@/app/api/lecturerService/dashboard/overView';
 import { IOverview } from "@/app/types/lecturer/dashboard/overview.type";
+import { getOverview } from "@/app/api/lecturerService/dashboard/overview";
 
 export const useGetOverview = () => {
     return useQuery<IOverview>({
