89a26b4be1f98f9ec0ad003820677b07e505f456
quangvungo <quangvu221004@gmail.com>
2026-04-12 11:42:17 +0700

    update

--- hooks/lecturer/dashboard/useGetOverview.ts
+++ hooks/lecturer/dashboard/useGetOverview.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import { IOverview } from "@/app/types/lecturer/dashboard/overview.type";
 import { getOverview } from "@/app/api/lecturerService/dashboard/overview";
-
 export const useGetOverview = () => {
     return useQuery<IOverview>({
         queryKey: ['get-overview'],
